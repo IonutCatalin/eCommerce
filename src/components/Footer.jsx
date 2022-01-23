aad0bf89ee36ce2ee33999820723b1fe4b01aef2
IonutCatalin <38530986+IonutCatalin@users.noreply.github.com>
2022-01-23 12:26:16 +0200

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Pinterest, Twitter } from '@material-ui/icons';
+import { Facebook, Instagram, Pinterest, Twitter, Phone, Room, MailOutline } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -14,7 +14,7 @@ const Left = styled.div`
 
 const Logo = styled.h1``;
 const Desc = styled.p`
-    margin: 20px; 0px;    
+    margin: 0px 0px 30px 0px;    
 `;
 const SocialContainer = styled.div`
     display: flex;
@@ -25,12 +25,48 @@ const SocialIcon = styled.div`
     border-radius: 50%;
     color: white;
     background-color: #${props => props.color};
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
 `;
 
 const Center = styled.div`
-    flex: 1;`
+    flex: 1;
+    padding:20px;
+`
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display:flex;
+    flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+
 const Right = styled.div`
-    flex: 1;`
+    flex: 1;
+    padding:20px;
+`
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-item: center;
+`
+
+const Payment = styled.img`
+    width: 50%;
+`
 
 const Footer = () => {
   return (
@@ -56,8 +92,28 @@ const Footer = () => {
                 </SocialIcon>
             </SocialContainer>
           </Left>
-          <Center></Center>
-          <Right></Right>
+          <Center>
+              <Title>Useful Links</Title>
+              <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Man Fashion</ListItem>
+                <ListItem>Woman Fashion</ListItem>
+                <ListItem>Accessories</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+              </List>
+          </Center>
+          <Right>
+                <Title>Contact</Title>
+                <ContactItem><Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336</ContactItem>
+                <ContactItem><Phone style={{marginRight:"10px"}}/>+1 234 56 78</ContactItem>
+                <ContactItem><MailOutline style={{marginRight:"10px"}}/>contact@lama.dev</ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+          </Right>
       </Container>
   );
 };
